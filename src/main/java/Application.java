c3e2a8e53c442acead3fd600a592a4660a570d9b
dqqzz <zdavid2023.01@gmail.com>
2023-09-27 18:08:05 -0400

    updata  09/27/2023
    update 09/27/2023

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -8,6 +8,9 @@ import java.sql.*;
 import java.util.List;
 import java.util.Scanner;
 
+// Define an enum to store grocery items
+enum GroceryItem {APPLE, BANANA, TOMATO, ORANGE, CARROT}
+
 public class Application {
     public static void main(String[] args) throws SQLException {
         Connection conn = ConnectionSingleton.getConnection();
@@ -18,69 +21,88 @@ public class Application {
 
         Scanner scan = new Scanner(System.in);
         boolean exit = false;
-        while(!exit){
+        while (!exit) {
             System.out.println("Would you like to inventory: \n(1) Add new item \n(2) Query item \n(3) Delete item \n(4) Update quantity of item \n(5) View Inventory\n");
 
             int response = scan.nextInt();
 
-            if(response==1){
-                // add item to service class
-                System.out.println("(1) ADD: Enter item: ");
-                String item = scan.next();
+            if (response == 1) {
+                // Add item to service class
+                System.out.println("(1) ADD: Enter item:\n" +
+                        "(1) " + GroceryItem.APPLE + "\n" +
+                        "(2) " + GroceryItem.BANANA + "\n" +
+                        "(3) " + GroceryItem.TOMATO + "\n" +
+                        "(4) " + GroceryItem.ORANGE + "\n" +
+                        "(5) " + GroceryItem.CARROT + "\n");
+                int itemChoice = scan.nextInt();
+                GroceryItem item = GroceryItem.values()[itemChoice - 1];
+
                 System.out.println("(1) ADD: Enter price: ");
                 double price = scan.nextDouble();
                 System.out.println("(1) ADD: Enter quantity: ");
                 int quantity = scan.nextInt();
-                Inventory inventory = new Inventory(item, price, quantity);
+                Inventory inventory = new Inventory(item.toString(), price, quantity);
 
                 System.out.println("\n");
                 inventoryService.addItem(inventory);
-            }
-            else if(response == 2){
-                // query item from service class
-                System.out.println("(2) QUERY: Enter name of item:");
-                String item = scan.next();
-                Inventory inventory = inventoryService.getItemByName(item);
+            } else if (response == 2) {
+                // Query item from service class
+                System.out.println("(2) QUERY: Enter name of item:\n" +
+                        "(1) " + GroceryItem.APPLE + "\n" +
+                        "(2) " + GroceryItem.BANANA + "\n" +
+                        "(3) " + GroceryItem.TOMATO + "\n" +
+                        "(4) " + GroceryItem.ORANGE + "\n" +
+                        "(5) " + GroceryItem.CARROT + "\n");
+                int itemChoice = scan.nextInt();
+                GroceryItem item = GroceryItem.values()[itemChoice - 1];
+
+                Inventory inventory = inventoryService.getItemByName(item.toString());
 
                 System.out.println("\n");
                 System.out.println(inventory);
-            }
-            else if(response == 3){
-                // delete item using service class
-                System.out.println("(3) DELETE: Enter item: ");
-                String item = scan.next();
+            } else if (response == 3) {
+                // Delete item using service class
+                System.out.println("(3) DELETE: Enter item:\n" +
+                        "(1) " + GroceryItem.APPLE + "\n" +
+                        "(2) " + GroceryItem.BANANA + "\n" +
+                        "(3) " + GroceryItem.TOMATO + "\n" +
+                        "(4) " + GroceryItem.ORANGE + "\n" +
+                        "(5) " + GroceryItem.CARROT + "\n");
+                int itemChoice = scan.nextInt();
+                GroceryItem item = GroceryItem.values()[itemChoice - 1];
 
                 System.out.println("\n");
-                inventoryService.deleteItem(item);
-            }
-            else if(response == 4){
-                // update item using service class
-                System.out.println("(4) UPDATE: Enter item: ");
-                String item = scan.next();
-//                System.out.println("(4) UPDATE: Enter price: ");
-//                double price = scan.nextDouble();
+                inventoryService.deleteItem(item.toString());
+            } else if (response == 4) {
+                // Update item using service class
+                System.out.println("(4) UPDATE: Enter item:\n" +
+                        "(1) " + GroceryItem.APPLE + "\n" +
+                        "(2) " + GroceryItem.BANANA + "\n" +
+                        "(3) " + GroceryItem.TOMATO + "\n" +
+                        "(4) " + GroceryItem.ORANGE + "\n" +
+                        "(5) " + GroceryItem.CARROT + "\n");
+                int itemChoice = scan.nextInt();
+                GroceryItem item = GroceryItem.values()[itemChoice - 1];
+
                 System.out.println("(4) UPDATE: Enter quantity: ");
                 int quantity = scan.nextInt();
-                Inventory inventory = new Inventory(item , quantity);
+                Inventory inventory = new Inventory(item.toString(), quantity);
 
                 System.out.println("\n");
                 inventoryService.updateItem(inventory);
-            }
-            else if(response == 5){
-                // query all items from service class
+            } else if (response == 5) {
+                // Query all items from service class
                 System.out.println("(5) QUERY: All items:\n");
                 inventoryService.getAllItems();
                 List<Inventory> inventoryList = inventoryService.getAllItems();
 
                 System.out.println("\n");
                 System.out.println(inventoryList);
-            }
-            else{
-                // invalid choice
+            } else {
+                // Invalid choice
                 System.out.println("\n");
                 System.out.println("Invalid choice: '" + response + "'\n");
             }
-
         }
     }
 }
--- src/main/resources/Tables.sql
+++ src/main/resources/Tables.sql
@@ -1,6 +1,18 @@
---drop table grocery_store if exists;
-create table if not exists grocery_store(Item varchar(255), Price double, Quantity int);
+drop table grocery_store if exists;
+create table if not exists grocery_store(Item varchar(255) not null primary key , Price double, Quantity int);
 --insert into grocery_store (Item, Price, Quantity) values ('apple', 1.00, 10);
 --insert into grocery_store (Item, Price, Quantity) values ('banana', 1.50, 12);
 ----insert into grocery_store (Item, Price, Quantity) values ('milk', 4.00, 20);
-
+CREATE TABLE IF NOT EXISTS grocery_info (
+    item_id INT unique ,
+    Item VARCHAR(255) NOT NULL primary key,
+    farm_name VARCHAR(255),
+    brand VARCHAR(255),
+    organized BOOLEAN,
+    contact VARCHAR(255)
+);
+insert into grocery_info(item_id, Item, farm_name, brand, organized,contact)values (1, 'APPLE','farm_1','new','111-222-2222');
+insert into grocery_info(item_id, Item, farm_name, brand, organized,contact)values (2, 'BANANA','farm_2','new','222-222-2222');
+insert into grocery_info(item_id, Item, farm_name, brand, organized,contact)values (3, 'ORANGE','farm_3','new','333-222-2222');
+insert into grocery_info(item_id, Item, farm_name, brand, organized,contact)values (4, 'TOMATO','farm_4','new','555-222-2222');
+insert into grocery_info(item_id, Item, farm_name, brand, organized,contact)values (5, 'CARROT','farm_5','new','');
