79a40136c6ec21805397b132ac1bb14157cb6fd2
dqqzz <zdavid2023.01@gmail.com>
2023-09-30 16:23:48 -0400

    javadocs and readme
    javadocs and readme

--- src/main/java/Controller/Controller.java
+++ src/main/java/Controller/Controller.java
@@ -13,32 +13,40 @@ import io.javalin.http.Context;
 import java.sql.SQLException;
 import java.util.List;
 
+/**
+ * Controller class responsible for handling HTTP requests and managing interactions between the API and services.
+ */
 public class Controller {
-    InventoryService inventoryService;
+    private InventoryService inventoryService;
 //    CustomerService customerService;
 
-
-
+    /**
+     * Initializes the Controller with an InventoryService.
+     *
+     * @param inventoryService The InventoryService to be used for managing inventory-related operations.
+     */
     public Controller(InventoryService inventoryService) {
         this.inventoryService = inventoryService;
     }
 
-//    public ControllerCustomer(CustomerService customerService) { this.customerService = customerService;}
+    //    public ControllerCustomer(CustomerService customerService) { this.customerService = customerService;}
 
-
+    /**
+     * Retrieves the Javalin API instance for this controller.
+     *
+     * @return The Javalin API instance.
+     */
     public Javalin getAPI() {
         Javalin app = Javalin.create();
 
-//        INVENTORY interactions
+        // INVENTORY interactions
         app.get("/inventory", this::getAllInventoryHandler);
         app.get("/inventory/{item}", this::getInventoryByNameHandler);
         app.get("/inventory/type/{type}", this::getAllInventoryByTypeHandler);
         app.post("/inventory", this::postInventoryHandler);
         app.put("/inventory", this::putInventoryHandler);
 
-//        app.delete("/inventory/{item}", this::deleteInventoryHandler);
-
-//        CUSTOMER interactions
+        // CUSTOMER interactions
         app.get("/customer", this::getAllCustomersHandler);
         app.get("/customer/city/{city}", this::getAllCustomersByCityHandler);
         app.post("/customer", this::postCustomerHandler);
@@ -47,120 +55,108 @@ public class Controller {
         return app;
     }
 
-
-
-
-    private void getAllInventoryHandler(Context context){
+    private void getAllInventoryHandler(Context context) {
         List<Inventory> inventoryList = inventoryService.getAllItems();
         context.json(inventoryList);
     }
-    private void getInventoryByNameHandler(Context context){
+
+    private void getInventoryByNameHandler(Context context) {
         String item = context.pathParam("item");
         Inventory inventory = inventoryService.getItemByName(item);
-        if(inventory == null){
+        if (inventory == null) {
             context.status(404);
-        }else{
+        } else {
             context.json(inventory);
         }
     }
 
     /**
-     * Retrieve all inventory by type
-     * @param context
+     * Retrieve all inventory by type.
+     *
+     * @param context The HTTP context.
      */
-    private void getAllInventoryByTypeHandler(Context context){
+    private void getAllInventoryByTypeHandler(Context context) {
         String type = context.pathParam("type");
-        List<Inventory> inventory= inventoryService.getAllItemsByType(type);
-        if(inventory == null){
+        List<Inventory> inventory = inventoryService.getAllItemsByType(type);
+        if (inventory == null) {
             context.status(404);
-        }else{
+        } else {
             context.json(inventory);
         }
     }
-    private void postInventoryHandler(Context context){
+
+    private void postInventoryHandler(Context context) {
         ObjectMapper om = new ObjectMapper();
         try {
             Inventory f = om.readValue(context.body(), Inventory.class);
             inventoryService.addItem(f);
-//            resource created response
+            // resource created response
             context.status(201);
-        }catch(JsonProcessingException e){
-//            if the json couldn't be processed, then the user sent us a faulty JSON,
-//            so return a 400
+        } catch (JsonProcessingException e) {
+            // if the JSON couldn't be processed, then the user sent us a faulty JSON,
+            // so return a 400
             e.printStackTrace();
             context.status(400);
         }
-
     }
 
-    private void putInventoryHandler(Context context){
+    private void putInventoryHandler(Context context) {
         ObjectMapper om = new ObjectMapper();
         try {
             Inventory f = om.readValue(context.body(), Inventory.class);
             inventoryService.updateItem(f);
-//            resource created response
+            // resource created response
             context.status(201);
-        }catch(JsonProcessingException e){
-//            if the json couldn't be processed, then the user sent us a faulty JSON,
-//            so return a 400
+        } catch (JsonProcessingException e) {
+            // if the JSON couldn't be processed, then the user sent us a faulty JSON,
+            // so return a 400
             e.printStackTrace();
             context.status(400);
         }
-
     }
 
-
-//Delete function is not desired,but passed the test.
-//    private void deleteInventoryHandler(Context context){
-//        String item = context.pathParam("item");
-//        inventoryService.deleteItem(item);
-//        Inventory inventory = inventoryService.getItemByName(item);
-//        if(inventory == null){
-//            context.status(404);
-//        }else{
-//            context.json(inventory);
-//        }
-//    }
-
     /**
-     * postCustomerHandler, putCustomerHandler, not working
+     * postCustomerHandler, putCustomerHandler, not working.
      */
-    private void getAllCustomersHandler(Context context){
+    private void getAllCustomersHandler(Context context) {
         List<Customer> customerList = inventoryService.getAllCustomers();
         System.out.println(customerList);
         context.json(customerList);
     }
-    private void getAllCustomersByCityHandler(Context context){
+
+    private void getAllCustomersByCityHandler(Context context) {
         String city = context.pathParam("city");
-        List<Customer> customerByCityList= inventoryService.getAllCustomersByCity(city);
-        if(customerByCityList == null){
+        List<Customer> customerByCityList = inventoryService.getAllCustomersByCity(city);
+        if (customerByCityList == null) {
             context.status(404);
-        }else{
+        } else {
             context.json(customerByCityList);
         }
     }
+
     private void postCustomerHandler(Context context) throws JsonProcessingException {
         ObjectMapper om = new ObjectMapper();
-        try{
+        try {
             Customer c = om.readValue(context.body(), Customer.class);
             inventoryService.addCustomer(c);
-//            resource created response
+            // resource created response
             context.status(201);
 
         } catch (JsonMappingException e) {
             throw new RuntimeException(e);
         }
     }
+
     private void putCustomerHandler(Context context) {
         ObjectMapper om = new ObjectMapper();
         try {
             Customer c = om.readValue(context.body(), Customer.class);
             inventoryService.updateCustomer(c);
-//            resource created response
+            // resource created response
             context.status(201);
-        }catch(JsonProcessingException e){
-//            if the json couldn't be processed, then the user sent us a faulty JSON,
-//            so return a 400
+        } catch (JsonProcessingException e) {
+            // if the JSON couldn't be processed, then the user sent us a faulty JSON,
+            // so return a 400
             e.printStackTrace();
             context.status(400);
         }
