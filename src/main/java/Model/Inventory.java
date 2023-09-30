aac280bd11877ca2d8fdd3c19cb1aa6571487b99
dqqzz <zdavid2023.01@gmail.com>
2023-09-30 16:45:03 -0400

    test-1
    test-1

--- src/main/java/Model/Inventory.java
+++ src/main/java/Model/Inventory.java
@@ -3,15 +3,13 @@ package Model;
 import Util.ConnectionSingleton;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.Objects;
-import java.util.PrimitiveIterator;
 
-public class Inventory{
+/**
+ * The Inventory class represents information about an inventory item.
+ */
+public class Inventory {
     private String item;
-    private  String type;
+    private String type;
     private double price;
     private int quantity;
     private int item_id;
@@ -19,57 +17,212 @@ public class Inventory{
     private String brand;
     private String contact;
 
+    // Get a database connection using a Singleton pattern.
     Connection conn = ConnectionSingleton.getConnection();
 
-    public Inventory(){
+    /**
+     * Default constructor for the Inventory class.
+     */
+    public Inventory() {}
 
-    }
-    public Inventory(String item, String type, double price, int quantity,
-                     int item_id, String farm_name, String brand, String contact){
+    /**
+     * Constructs an Inventory object with specified attributes.
+     *
+     * @param item      The name of the inventory item.
+     * @param type      The type of the inventory item.
+     * @param price     The price of the inventory item.
+     * @param quantity  The quantity of the inventory item.
+     * @param item_id   The unique identifier for the inventory item.
+     * @param farm_name The name of the farm associated with the item.
+     * @param brand     The brand of the item.
+     * @param contact   The contact information for the item.
+     */
+    public Inventory(String item, String type, double price, int quantity, int item_id, String farm_name, String brand, String contact) {
         this.item = item;
-        this.type=type;
-        this.price= price;
+        this.type = type;
+        this.price = price;
         this.quantity = quantity;
-        this.item_id=item_id;
-        this.farm_name=farm_name;
-        this.brand=brand;
-        this.contact=contact;
+        this.item_id = item_id;
+        this.farm_name = farm_name;
+        this.brand = brand;
+        this.contact = contact;
     }
-    public Inventory(String item, int quantity){
+
+    /**
+     * Constructs an Inventory object with the item name and quantity.
+     *
+     * @param item     The name of the inventory item.
+     * @param quantity The quantity of the inventory item.
+     */
+    public Inventory(String item, int quantity) {
         this.item = item;
         this.quantity = quantity;
     }
-    public Inventory(String item, String type, double price, int quantity){
+
+    /**
+     * Constructs an Inventory object with specified attributes, excluding item_id, farm_name, brand, and contact.
+     *
+     * @param item     The name of the inventory item.
+     * @param type     The type of the inventory item.
+     * @param price    The price of the inventory item.
+     * @param quantity The quantity of the inventory item.
+     */
+    public Inventory(String item, String type, double price, int quantity) {
         this.item = item;
-        this.type=type;
+        this.type = type;
+        this.price = price;
         this.quantity = quantity;
-        this.price=price;
     }
+
+    /**
+     * Get the name of the inventory item.
+     *
+     * @return The item name.
+     */
     public String getItem() {
         return item;
     }
 
+    /**
+     * Get the price of the inventory item.
+     *
+     * @return The item price.
+     */
     public double getPrice() {
         return price;
     }
 
+    /**
+     * Get the quantity of the inventory item.
+     *
+     * @return The item quantity.
+     */
     public int getQuantity() {
         return quantity;
     }
 
+    /**
+     * Set the name of the inventory item.
+     *
+     * @param item The item name.
+     */
     public void setItem(String item) {
         this.item = item;
     }
 
+    /**
+     * Set the price of the inventory item.
+     *
+     * @param price The item price.
+     */
     public void setPrice(double price) {
         this.price = price;
     }
 
+    /**
+     * Set the quantity of the inventory item.
+     *
+     * @param quantity The item quantity.
+     */
     public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
 
+    /**
+     * Get the unique identifier for the inventory item.
+     *
+     * @return The item's unique identifier.
+     */
+    public int getItem_id() {
+        return item_id;
+    }
 
+    /**
+     * Set the unique identifier for the inventory item.
+     *
+     * @param item_id The item's unique identifier.
+     */
+    public void setItem_id(int item_id) {
+        this.item_id = item_id;
+    }
+
+    /**
+     * Get the name of the farm associated with the item.
+     *
+     * @return The farm name.
+     */
+    public String getFarm_name() {
+        return farm_name;
+    }
+
+    /**
+     * Set the name of the farm associated with the item.
+     *
+     * @param farm_name The farm name.
+     */
+    public void setFarm_name(String farm_name) {
+        this.farm_name = farm_name;
+    }
+
+    /**
+     * Get the brand of the item.
+     *
+     * @return The item brand.
+     */
+    public String getBrand() {
+        return brand;
+    }
+
+    /**
+     * Set the brand of the item.
+     *
+     * @param brand The item brand.
+     */
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    /**
+     * Get the contact information for the item.
+     *
+     * @return The item contact information.
+     */
+    public String getContact() {
+        return contact;
+    }
+
+    /**
+     * Set the contact information for the item.
+     *
+     * @param contact The item contact information.
+     */
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    /**
+     * Get the type of the inventory item.
+     *
+     * @return The item type.
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * Set the type of the inventory item.
+     *
+     * @param type The item type.
+     */
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    /**
+     * Override of the toString method to provide a string representation of the Inventory object.
+     *
+     * @return A string representation of the Inventory object.
+     */
     @Override
     public String toString() {
         return "Inventory{" +
@@ -82,45 +235,5 @@ public class Inventory{
                 ", brand='" + brand + '\'' +
                 ", contact='" + contact + '\'' +
                 '}';
-    }
-
-    public int getItem_id() {
-        return item_id;
-    }
-
-    public void setItem_id(int item_id) {
-        this.item_id = item_id;
-    }
-
-    public String getFarm_name() {
-        return farm_name;
-    }
-
-    public void setFarm_name(String farm_name) {
-        this.farm_name = farm_name;
-    }
-
-    public String getBrand() {
-        return brand;
-    }
-
-    public void setBrand(String brand) {
-        this.brand = brand;
-    }
-
-    public String getContact() {
-        return contact;
-    }
-
-    public void setContact(String contact) {
-        this.contact = contact;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
     }
 }
