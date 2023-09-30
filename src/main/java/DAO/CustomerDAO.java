a0e7ea3a70431927d7edaf96286a6eb36923cbfa
dqqzz <zdavid2023.01@gmail.com>
2023-09-30 16:30:54 -0400

    change test
    change test

--- src/main/java/DAO/CustomerDAO.java
+++ src/main/java/DAO/CustomerDAO.java
@@ -10,18 +10,26 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Data Access Object (DAO) for managing customer-related database operations.
+ */
 public class CustomerDAO {
 
     private Connection conn;
 
-    public CustomerDAO(Connection conn){
+    /**
+     * Constructs a CustomerDAO with a database connection.
+     *
+     * @param conn The database connection.
+     */
+    public CustomerDAO(Connection conn) {
         this.conn = conn;
     }
 
-
     /**
-     * Add customer to customer table via customer list
-     * @param customer
+     * Add a customer to the customer table.
+     *
+     * @param customer The Customer object to be added.
      */
     public void addCustomer(Customer customer) {
         try {
@@ -32,84 +40,84 @@ public class CustomerDAO {
             ps.setString(3, customer.getCustomerCity());
             ps.setInt(4, customer.getCustomerZip());
             ps.executeUpdate();
-        }catch (SQLException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
     /**
-     * Delete a customer from customer table
-     * @param customer
+     * Delete a customer from the customer table.
+     *
+     * @param customer The Customer object to be deleted.
      */
     public void deleteCustomer(Customer customer) {
-        try{
+        try {
             PreparedStatement ps = conn.prepareStatement("delete customer where customer_id = ?");
             ps.setInt(1, customer.getCustomerId());
             ps.executeUpdate();
-        }catch (SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
         }
-
     }
 
     /**
-     * Update customer information: (first name, last name, city, zip code) via customer_id
-     * @param customer
+     * Update customer information (first name, last name, city, zip code) via customer_id.
+     *
+     * @param customer The Customer object containing updated information.
      */
     public void updateCustomer(Customer customer) {
-        try{
-            PreparedStatement ps = conn.prepareStatement("update customer set first_name=? last_name=? city=? zip_code=? where customer_id=?");
+        try {
+            PreparedStatement ps = conn.prepareStatement("update customer set first_name=?, last_name=?, city=?, zip_code=? where customer_id=?");
             ps.setString(1, customer.getCustomerFirstName());
             ps.setString(2, customer.getCustomerLastName());
             ps.setString(3, customer.getCustomerCity());
             ps.setInt(4, customer.getCustomerZip());
             ps.executeUpdate();
-        }catch(SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
     /**
-     * Get customer record via customer id
-     * @param customerId
-     * @return
+     * Get a customer's record by customer id.
+     *
+     * @param customerId The ID of the customer to retrieve.
+     * @return The Customer object representing the retrieved customer, or null if not found.
      */
     public Customer getCustomerById(int customerId) {
-        try{
+        try {
             PreparedStatement ps = conn.prepareStatement("select * from customer where customer_id=?");
             ps.setInt(1, customerId);
             ResultSet rs = ps.executeQuery();
-            if(rs.next()){
+            if (rs.next()) {
                 int dbCustomerId = rs.getInt("customer_id");
                 String dbCustomerFirstName = rs.getString("first_name");
                 String dbCustomerLastName = rs.getString("last_name");
                 String dbCity = rs.getString("city");
                 int dbZip = rs.getInt("zip_code");
                 return new Customer(dbCustomerId, dbCustomerFirstName, dbCustomerLastName, dbCity, dbZip);
-
             }
 
-        }catch(SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
         }
         return null;
     }
 
     /**
-     * Get customer record b
-     * @param customerFirstName
-     * @return
+     * Get a list of customers with a specific first name.
+     *
+     * @param customerFirstName The first name of the customers to retrieve.
+     * @return A List of Customer objects with matching first names.
      */
     public List<Customer> getAllCustomersByFirstName(String customerFirstName) {
         List<Customer> customerList = new ArrayList<>();
-        try{
+        try {
             PreparedStatement ps = conn.prepareStatement("select * from customer where first_name=?");
             ps.setString(1, customerFirstName);
-
             ResultSet rs = ps.executeQuery();
 
-
-            while(rs.next()){
+            while (rs.next()) {
                 int dbCustomerId = rs.getInt("customer_id");
                 String dbCustomerFirstName = rs.getString("first_name");
                 String dbCustomerLastName = rs.getString("last_name");
@@ -117,23 +125,26 @@ public class CustomerDAO {
                 int dbCustomerZip = rs.getInt("zip_code");
                 Customer dbCustomerList = new Customer(dbCustomerId, dbCustomerFirstName, dbCustomerLastName, dbCustomerCity, dbCustomerZip);
                 customerList.add(dbCustomerList);
-
-
             }
-
-        }catch(SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
         }
         return customerList;
     }
 
+    /**
+     * Get a list of customers with a specific last name.
+     *
+     * @param customerLastName The last name of the customers to retrieve.
+     * @return A List of Customer objects with matching last names.
+     */
     public List<Customer> getAllCustomersByLastName(String customerLastName) {
         List<Customer> customerList = new ArrayList<>();
-        try{
+        try {
             PreparedStatement ps = conn.prepareStatement("select * from customer where last_name=?");
             ps.setString(1, customerLastName);
             ResultSet rs = ps.executeQuery();
-            while(rs.next()){
+            while (rs.next()) {
                 int dbCustomerId = rs.getInt("customer_id");
                 String dbCustomerFirstName = rs.getString("first_name");
                 String dbCustomerLastName = rs.getString("last_name");
@@ -141,54 +152,60 @@ public class CustomerDAO {
                 int dbCustomerZip = rs.getInt("zip_code");
                 Customer dbCustomerList = new Customer(dbCustomerId, dbCustomerFirstName, dbCustomerLastName, dbCustomerCity, dbCustomerZip);
                 customerList.add(dbCustomerList);
-
             }
-
-        }catch(SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
         }
         return customerList;
     }
 
+    /**
+     * Get a list of all customers.
+     *
+     * @return A List of all Customer objects in the database.
+     */
     public List<Customer> getAllCustomers() {
         List<Customer> customerList = new ArrayList<>();
-        try{
+        try {
             PreparedStatement ps = conn.prepareStatement("select * from customer");
-
             ResultSet rs = ps.executeQuery();
-            while(rs.next()){
+            while (rs.next()) {
                 int dbCustomerId = rs.getInt("customer_id");
                 String dbCustomerFirstName = rs.getString("first_name");
                 String dbCustomerLastName = rs.getString("last_name");
                 String dbCustomerCity = rs.getString("city");
                 int dbCustomerZip = rs.getInt("zip_code");
-
                 Customer dbCustomerList = new Customer(dbCustomerId, dbCustomerFirstName, dbCustomerLastName, dbCustomerCity, dbCustomerZip);
                 customerList.add(dbCustomerList);
             }
-        }catch(SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
         }
         return customerList;
     }
+
+    /**
+     * Get a list of customers from a specific city.
+     *
+     * @param city The name of the city.
+     * @return A List of Customer objects from the specified city.
+     */
     public List<Customer> getAllCustomersByCity(String city) {
         List<Customer> customerList = new ArrayList<>();
-        try{
+        try {
             PreparedStatement ps = conn.prepareStatement("select * from customer where city=?");
             ps.setString(1, city);
-
             ResultSet rs = ps.executeQuery();
-            while(rs.next()){
+            while (rs.next()) {
                 int dbCustomerId = rs.getInt("customer_id");
                 String dbCustomerFirstName = rs.getString("first_name");
                 String dbCustomerLastName = rs.getString("last_name");
                 String dbCustomerCity = rs.getString("city");
                 int dbCustomerZip = rs.getInt("zip_code");
-
                 Customer dbCustomerList = new Customer(dbCustomerId, dbCustomerFirstName, dbCustomerLastName, dbCustomerCity, dbCustomerZip);
                 customerList.add(dbCustomerList);
             }
-        }catch(SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
         }
         return customerList;
--- src/main/java/Model/Customer.java
+++ src/main/java/Model/Customer.java
@@ -2,69 +2,149 @@ package Model;
 
 import java.util.List;
 
+/**
+ * The Customer class represents customer information.
+ */
 public class Customer {
     private int customerId;
     private String customerFirstName;
     private String customerLastName;
-    private String customerAddress;
     private String customerCity;
     private int customerZip;
 
+    /**
+     * Default constructor for the Customer class.
+     */
     public Customer() {}
-    public Customer (int customerId, String customerFirstName, String customerLastName, String customerCity, int customerZip){
+
+    /**
+     * Constructs a Customer object with specified attributes.
+     *
+     * @param customerId       The unique identifier for the customer.
+     * @param customerFirstName The first name of the customer.
+     * @param customerLastName  The last name of the customer.
+     * @param customerCity      The city where the customer lives.
+     * @param customerZip       The ZIP code of the customer's address.
+     */
+    public Customer(int customerId, String customerFirstName, String customerLastName, String customerCity, int customerZip) {
         this.customerId = customerId;
         this.customerFirstName = customerFirstName;
         this.customerLastName = customerLastName;
         this.customerCity = customerCity;
         this.customerZip = customerZip;
-
     }
 
     /**
-     * overloaded method for adding to table w/ auto_increment?
-     * @param customerFirstName
-     * @param customerLastName     *
-     * @param customerCity
-     * @param customerZip
+     * Constructs a Customer object with specified attributes, excluding customerId.
+     * This constructor is typically used when adding a new customer to a table with auto-incremented IDs.
+     *
+     * @param customerFirstName The first name of the customer.
+     * @param customerLastName  The last name of the customer.
+     * @param customerCity      The city where the customer lives.
+     * @param customerZip       The ZIP code of the customer's address.
      */
-    public Customer (String customerFirstName, String customerLastName, String customerCity, int customerZip){
-
+    public Customer(String customerFirstName, String customerLastName, String customerCity, int customerZip) {
         this.customerFirstName = customerFirstName;
         this.customerLastName = customerLastName;
         this.customerCity = customerCity;
         this.customerZip = customerZip;
-
     }
 
+    /**
+     * Get the customer's unique identifier (ID).
+     *
+     * @return The customer's ID.
+     */
     public int getCustomerId() {
         return customerId;
     }
 
+    /**
+     * Set the customer's unique identifier (ID).
+     *
+     * @param customerId The customer's ID.
+     */
     public void setCustomerId(int customerId) {
         this.customerId = customerId;
     }
 
-    public void setCustomerFirstName(String customerFirstName) { this.customerFirstName = customerFirstName; }
-    public String getCustomerFirstName() { return customerFirstName; }
-    public void setCustomerLastName(String customerLastName) { this.customerLastName = customerLastName; }
-    public String getCustomerLastName() { return customerLastName; }
+    /**
+     * Get the customer's first name.
+     *
+     * @return The customer's first name.
+     */
+    public String getCustomerFirstName() {
+        return customerFirstName;
+    }
 
+    /**
+     * Set the customer's first name.
+     *
+     * @param customerFirstName The customer's first name.
+     */
+    public void setCustomerFirstName(String customerFirstName) {
+        this.customerFirstName = customerFirstName;
+    }
+
+    /**
+     * Get the customer's last name.
+     *
+     * @return The customer's last name.
+     */
+    public String getCustomerLastName() {
+        return customerLastName;
+    }
+
+    /**
+     * Set the customer's last name.
+     *
+     * @param customerLastName The customer's last name.
+     */
+    public void setCustomerLastName(String customerLastName) {
+        this.customerLastName = customerLastName;
+    }
+
+    /**
+     * Get the city where the customer lives.
+     *
+     * @return The customer's city.
+     */
     public String getCustomerCity() {
         return customerCity;
     }
 
+    /**
+     * Set the city where the customer lives.
+     *
+     * @param customerCity The customer's city.
+     */
     public void setCustomerCity(String customerCity) {
         this.customerCity = customerCity;
     }
 
+    /**
+     * Get the ZIP code of the customer's address.
+     *
+     * @return The customer's ZIP code.
+     */
     public int getCustomerZip() {
         return customerZip;
     }
 
+    /**
+     * Set the ZIP code of the customer's address.
+     *
+     * @param customerZip The customer's ZIP code.
+     */
     public void setCustomerZip(int customerZip) {
         this.customerZip = customerZip;
     }
 
+    /**
+     * Override of the toString method to provide a string representation of the Customer object.
+     *
+     * @return A string representation of the Customer object.
+     */
     @Override
     public String toString() {
         return "Customer{" +
