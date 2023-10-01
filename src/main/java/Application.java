c5635c47cc079393cb0c46485af8ce14e870d26e
dqqzz <zdavid2023.01@gmail.com>
2023-10-01 00:11:19 -0400

    more tests
    more tests

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,6 +1,7 @@
 import Controller.Controller;
 import DAO.CustomerDAO;
 import DAO.InventoryDAO;
+import Exceptions.NoProductFoundException;
 import Model.Customer;
 import Model.Inventory;
 import Service.CustomerService;
@@ -11,6 +12,9 @@ import java.sql.*;
 import java.util.List;
 import java.util.Scanner;
 
+/**
+ * The Application class is the entry point for the grocery store management application.
+ */
 public class Application {
     public static void main(String[] args) throws SQLException {
         Connection conn = ConnectionSingleton.getConnection();
@@ -18,21 +22,15 @@ public class Application {
         InventoryService inventoryService = new InventoryService(inventoryDAO);
         Controller controller = new Controller(inventoryService);
 
-
-
-        // customer flow; controller layer what do?
+        // Customer flow; controller layer what do?
         CustomerDAO customerDAO = new CustomerDAO(conn);
         CustomerService customerService = new CustomerService(customerDAO);
-//        Controller controller2 = CustomerController(customerService);
-
-
 
         controller.getAPI().start();
 
-
         Scanner scan = new Scanner(System.in);
         boolean exit = false;
-        while(!exit){
+        while (!exit) {
             System.out.println("\n[grocery_store] inventory: \n" +
                     "\n(1) Add new item " +
                     "\n(2) Query item " +
@@ -49,9 +47,9 @@ public class Application {
 
             int response = scan.nextInt();
 
-            //            INVENTORY table UI
-            if(response==1){
-                // add item to service class
+            // INVENTORY table UI
+            if (response == 1) {
+                // Add item to service class
                 System.out.println("(1) ADD: Enter item: ");
                 String item = scan.next();
                 System.out.println("(1) ADD: Enter type: ");
@@ -64,60 +62,56 @@ public class Application {
 
                 System.out.println("\n");
                 inventoryService.addItem(inventory);
-            }
-            else if(response == 2){
-                // query item from service class
+            } else if (response == 2) {
+                // Query item from service class
                 System.out.println("(2) QUERY: Enter name of item:");
                 String item = scan.next();
                 Inventory inventory = inventoryService.getItemByName(item);
 
                 System.out.println("\n");
                 System.out.println(inventory);
-            }
-            else if(response == 3){
-                // delete item using service class
+            } else if (response == 3) {
+                // Delete item using service class
                 System.out.println("(3) DELETE: Enter item: ");
                 String item = scan.next();
 
                 System.out.println("\n");
                 inventoryService.deleteItem(item);
-            }
-            else if(response == 4){
-                // update item using service class
+            } else if (response == 4) {
+                // Update item using service class
                 System.out.println("(4) UPDATE: Enter item: ");
                 String item = scan.next();
                 System.out.println("(4) UPDATE: Enter price: ");
                 double price = scan.nextDouble();
                 System.out.println("(4) UPDATE: Enter quantity: ");
                 int quantity = scan.nextInt();
-                Inventory inventory = new Inventory(item , quantity);
+                Inventory inventory = new Inventory(item, quantity);
 
                 System.out.println("\n");
                 inventoryService.updateItem(inventory);
-            }
-            else if(response == 5){
-                // query all items from service class
+            } else if (response == 5) {
+                // Query all items from service class
                 System.out.println("(5) QUERY: All items:\n");
                 inventoryService.getAllItems();
                 List<Inventory> inventoryList = inventoryService.getAllItems();
 
                 System.out.println("\n");
                 System.out.println(inventoryList);
-            }
-            else if(response == 6){
-                // query type from service class
+            } else if (response == 6) {
+                // Query type from service class
                 System.out.println("(6) QUERY: Enter type: \n" +
                         "( produce / meat / baby_products / international / candy / animal_products / hygiene / paper_products )\n");
                 String type = scan.next();
-//                List<Inventory> inventory = inventoryService.getAllItemsByType(type);
 
                 System.out.println("\n");
-//                System.out.println(inventory);
+                // Uncomment the code below to display the list of items by type.
+                // List<Inventory> inventory = inventoryService.getAllItemsByType(type);
+                // System.out.println(inventory);
             }
 
-            //           CUSTOMER UI
-            else if(response==7){
-                // add customer to service class
+            // CUSTOMER UI
+            else if (response == 7) {
+                // Add customer to service class
                 System.out.println("(7) ADD CUSTOMER: Enter first name: ");
                 String first_name = scan.next();
                 System.out.println("(7) ADD CUSTOMER: Enter last name: ");
@@ -131,58 +125,45 @@ public class Application {
 
                 System.out.println("\n");
                 customerService.addCustomer(customer);
-            }
-
-
-
-
-            else if(response == 8){
-                // query all customers
+            } else if (response == 8) {
+                // Query all customers
                 System.out.println("(8) QUERY: All customers:\n");
                 customerService.getAllCustomers();
                 List<Customer> customerList = customerService.getAllCustomers();
 
                 System.out.println("\n");
                 System.out.println(customerList);
-            }
-            else if(response == 9){
-                // get customer by *ID*
+            } else if (response == 9) {
+                // Get customer by *ID*
                 System.out.println("(9) QUERY: Enter customer id: \n");
                 int customerId = scan.nextInt();
                 Customer customer = customerService.getCustomerById(customerId);
                 System.out.println(customer);
-
-            }
-            else if(response == 10){
-                // get customer by *FIRST NAME*
+            } else if (response == 10) {
+                // Get customer by *FIRST NAME*
                 System.out.println("(10) QUERY: Enter customer first name: \n");
                 String customerFirstName = scan.next();
                 List<Customer> customerList = customerService.getAllCustomersByFirstName(customerFirstName);
                 System.out.println(customerList);
-
-            }
-            else if(response == 11){
-                // get customer by *LAST NAME*
+            } else if (response == 11) {
+                // Get customer by *LAST NAME*
                 System.out.println("(11) QUERY: Enter customer last name: \n");
                 String customerLastName = scan.next();
                 List<Customer> customerList = customerService.getAllCustomersByLastName(customerLastName);
                 System.out.println(customerList);
-
-            }else if(response == 12){
-                // query all customerS from a *CITY*
+            } else if (response == 12) {
+                // Query all customers from a *CITY*
                 System.out.println("(12) QUERY: Enter city: \n");
                 String city = scan.next();
                 List<Customer> customerList = customerService.getAllCustomersByCity(city);
 
                 System.out.println("\n");
                 System.out.println(customerList);
-            }
-            else{
-                // invalid choice
+            } else {
+                // Invalid choice
                 System.out.println("\n");
                 System.out.println("Invalid choice: '" + response + "'\n");
             }
-
         }
     }
 }
--- src/main/java/DAO/InventoryDAO.java
+++ src/main/java/DAO/InventoryDAO.java
@@ -15,6 +15,11 @@ public class InventoryDAO {
 
     private Connection conn;
 
+    /**
+     * Constructs an InventoryDAO with a database connection.
+     *
+     * @param conn The database connection.
+     */
     public InventoryDAO(Connection conn) {
         this.conn = conn;
     }
@@ -22,51 +27,53 @@ public class InventoryDAO {
     /**
      * This method takes in an object inventory of type class Inventory to insert into
      * the SQL database table 'grocery_store', adding a record row of item name, price, and quantity.
-     * @param inventory
+     *
+     * @param inventory The Inventory object to be added.
      */
     public void addItem(Inventory inventory) {
         try {
-        PreparedStatement ps = conn.prepareStatement("insert into grocery_store (Item, Type, Price, Quantity) values (?, ?, ?, ?)");
-        ps.setString(1, inventory.getItem());
-        ps.setString(2, inventory.getType());
-        ps.setDouble(3, inventory.getPrice());
-        ps.setInt(4, inventory.getQuantity());
-        ps.executeUpdate();
-        }catch (SQLException e) {
+            PreparedStatement ps = conn.prepareStatement("insert into grocery_store (Item, Type, Price, Quantity) values (?, ?, ?, ?)");
+            ps.setString(1, inventory.getItem());
+            ps.setString(2, inventory.getType());
+            ps.setDouble(3, inventory.getPrice());
+            ps.setInt(4, inventory.getQuantity());
+            ps.executeUpdate();
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
     /**
-     * This method is used grab records from the table 'grocery_store' with the item name matching
+     * This method is used to grab records from the table 'grocery_store' with the item name matching
      * the passed parameter String, 'item'. These records are then stored into Java variables and added
-     * to the new Inventory object, dbInventory and returned.
-     * @param item
-     * @return
+     * to the new Inventory object, dbInventory, and returned.
+     *
+     * @param item The name of the item to retrieve.
+     * @return The Inventory object representing the retrieved item, or null if not found.
      */
     public Inventory getItemByName(String item) {
-        try{
-            PreparedStatement ps = conn.prepareStatement("SELECT * FROM grocery_store "+
-                                                "left join grocery_info "+
-                                                "ON grocery_store.item = grocery_info.item "+
-                                                 "WHERE grocery_store.item = ?");
+        try {
+            PreparedStatement ps = conn.prepareStatement("SELECT * FROM grocery_store " +
+                    "left join grocery_info " +
+                    "ON grocery_store.item = grocery_info.item " +
+                    "WHERE grocery_store.item = ?");
             ps.setString(1, item);
 
             ResultSet rs = ps.executeQuery();
-            if(rs.next()){
+            if (rs.next()) {
                 String dbItem = rs.getString("item");
                 String dbType = rs.getString("type");
                 double dbPrice = rs.getDouble("price");
                 int dbQuantity = rs.getInt("quantity");
-                int dbItem_id= rs.getInt("item_id");
+                int dbItem_id = rs.getInt("item_id");
                 String dbFarm_name = rs.getString("farm_name");
-                String dbBrand= rs.getString("brand");
+                String dbBrand = rs.getString("brand");
                 String dbContact = rs.getString("contact");
 
                 Inventory dbInventory = new Inventory(dbItem, dbType, dbPrice, dbQuantity, dbItem_id, dbFarm_name, dbBrand, dbContact);
                 return dbInventory;
             }
-        }catch(SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
         }
         return null;
@@ -75,81 +82,83 @@ public class InventoryDAO {
     /**
      * This method is used to delete a record from the table 'grocery_store' that matches the passed
      * String, 'item'.
-     * @param item
+     *
+     * @param item The name of the item to delete.
      */
     public void deleteItem(String item) {
-        try{
+        try {
             PreparedStatement ps = conn.prepareStatement("delete grocery_store where Item = ?");
             ps.setString(1, item);
             ps.executeUpdate();
-        }catch(SQLException e){
-         e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 
     /**
      * This method is used to update the quantity of a record in the table 'grocery_store', matching the item name
-     * passed in the object 'inventory'
+     * passed in the object 'inventory'.
      *
-     * @param inventory
+     * @param inventory The Inventory object containing the updated quantity.
      */
-    public void updateItem(Inventory inventory){
-        try{
+    public void updateItem(Inventory inventory) {
+        try {
             PreparedStatement ps = conn.prepareStatement("UPDATE grocery_store set quantity=? where Item = ?");
             ps.setInt(1, inventory.getQuantity());
             ps.setString(2, inventory.getItem());
             ps.executeUpdate();
-        }catch(SQLException e)
-        {e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-
-
     }
 
     /**
      * This method is used to retrieve all records from the table 'grocery_store', store them in an Inventory
      * object 'dbInventory', add each object as an element in an ArrayList, 'inventoryList', and return this
      * ArrayList.
-     * @return
+     *
+     * @return A List of Inventory objects representing all items in the database.
      */
     public List<Inventory> getAllItems() {
         List<Inventory> inventoryList = new ArrayList<>();
-        try{
-            PreparedStatement ps = conn.prepareStatement("SELECT * FROM grocery_store "+
-                    "left join grocery_info "+
+        try {
+            PreparedStatement ps = conn.prepareStatement("SELECT * FROM grocery_store " +
+                    "left join grocery_info " +
                     "ON grocery_store.item = grocery_info.item  ");
             ResultSet rs = ps.executeQuery();
-            while(rs.next()){
+            while (rs.next()) {
 
                 String dbItem = rs.getString("Item");
                 String dbType = rs.getString("Type");
                 double dbPrice = rs.getDouble("Price");
                 int dbQuantity = rs.getInt("Quantity");
-                int dbItem_id= rs.getInt("Item_id");
+                int dbItem_id = rs.getInt("Item_id");
                 String dbFarm_name = rs.getString("Farm_name");
-                String dbBrand= rs.getString("Brand");
+                String dbBrand = rs.getString("Brand");
                 String dbContact = rs.getString("Contact");
 
                 Inventory dbInventory = new Inventory(dbItem, dbType, dbPrice, dbQuantity, dbItem_id, dbFarm_name, dbBrand, dbContact);
                 inventoryList.add(dbInventory);
             }
-        }catch(SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
         }
         return inventoryList;
     }
 
     /**
-     * This method is used to retrieve all items by type
-     * @return
+     * This method is used to retrieve all items by type.
+     *
+     * @param type The type of items to retrieve.
+     * @return A List of Inventory objects representing items of the specified type.
      */
     public List<Inventory> getAllItemsByType(String type) {
         List<Inventory> inventoryList = new ArrayList<>();
-        try{
+        try {
             PreparedStatement ps = conn.prepareStatement("SELECT * FROM grocery_store where type=?");
             ps.setString(1, type);
             ResultSet rs = ps.executeQuery();
-            while(rs.next()){
+            while (rs.next()) {
 
                 String dbItem = rs.getString("item");
                 String dbType = rs.getString("type");
@@ -158,19 +167,26 @@ public class InventoryDAO {
                 Inventory dbInventory = new Inventory(dbItem, dbType, dbPrice, dbQuantity);
                 inventoryList.add(dbInventory);
             }
-        }catch(SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
         }
         return inventoryList;
     }
-    //                  customer TABLE API reroute
+
+    // Customer TABLE API reroute
+
+    /**
+     * Get a list of all customers.
+     *
+     * @return A List of all Customer objects in the database.
+     */
     public List<Customer> getAllCustomers() {
         List<Customer> customerList = new ArrayList<>();
-        try{
+        try {
             PreparedStatement ps = conn.prepareStatement("select * from customer");
 
             ResultSet rs = ps.executeQuery();
-            while(rs.next()){
+            while (rs.next()) {
                 int dbCustomerId = rs.getInt("customer_id");
                 String dbCustomerFirstName = rs.getString("first_name");
                 String dbCustomerLastName = rs.getString("last_name");
@@ -180,19 +196,26 @@ public class InventoryDAO {
                 Customer dbCustomerList = new Customer(dbCustomerId, dbCustomerFirstName, dbCustomerLastName, dbCustomerCity, dbCustomerZip);
                 customerList.add(dbCustomerList);
             }
-        }catch(SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
         }
         return customerList;
     }
+
+    /**
+     * Get a list of customers by city.
+     *
+     * @param city The city to filter by.
+     * @return A List of Customer objects in the specified city.
+     */
     public List<Customer> getAllCustomersByCity(String city) {
         List<Customer> customerList = new ArrayList<>();
-        try{
+        try {
             PreparedStatement ps = conn.prepareStatement("select * from customer where city=?");
             ps.setString(1, city);
 
             ResultSet rs = ps.executeQuery();
-            while(rs.next()){
+            while (rs.next()) {
                 int dbCustomerId = rs.getInt("customer_id");
                 String dbCustomerFirstName = rs.getString("first_name");
                 String dbCustomerLastName = rs.getString("last_name");
@@ -202,21 +225,27 @@ public class InventoryDAO {
                 Customer dbCustomerList = new Customer(dbCustomerId, dbCustomerFirstName, dbCustomerLastName, dbCustomerCity, dbCustomerZip);
                 customerList.add(dbCustomerList);
             }
-        }catch(SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
         }
         return customerList;
     }
+
+    /**
+     * Get a list of customers by first name.
+     *
+     * @param customerFirstName The first name to filter by.
+     * @return A List of Customer objects with the specified first name.
+     */
     public List<Customer> getAllCustomersByFirstName(String customerFirstName) {
         List<Customer> customerList = new ArrayList<>();
-        try{
+        try {
             PreparedStatement ps = conn.prepareStatement("select * from customer where first_name=?");
             ps.setString(1, customerFirstName);
 
             ResultSet rs = ps.executeQuery();
 
-
-            while(rs.next()){
+            while (rs.next()) {
                 int dbCustomerId = rs.getInt("customer_id");
                 String dbCustomerFirstName = rs.getString("first_name");
                 String dbCustomerLastName = rs.getString("last_name");
@@ -224,23 +253,26 @@ public class InventoryDAO {
                 int dbCustomerZip = rs.getInt("zip_code");
                 Customer dbCustomerList = new Customer(dbCustomerId, dbCustomerFirstName, dbCustomerLastName, dbCustomerCity, dbCustomerZip);
                 customerList.add(dbCustomerList);
-
-
             }
-
-        }catch(SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
         }
         return customerList;
     }
 
+    /**
+     * Get a list of customers by last name.
+     *
+     * @param customerLastName The last name to filter by.
+     * @return A List of Customer objects with the specified last name.
+     */
     public List<Customer> getAllCustomersByLastName(String customerLastName) {
         List<Customer> customerList = new ArrayList<>();
-        try{
+        try {
             PreparedStatement ps = conn.prepareStatement("select * from customer where last_name=?");
             ps.setString(1, customerLastName);
             ResultSet rs = ps.executeQuery();
-            while(rs.next()){
+            while (rs.next()) {
                 int dbCustomerId = rs.getInt("customer_id");
                 String dbCustomerFirstName = rs.getString("first_name");
                 String dbCustomerLastName = rs.getString("last_name");
@@ -248,17 +280,20 @@ public class InventoryDAO {
                 int dbCustomerZip = rs.getInt("zip_code");
                 Customer dbCustomerList = new Customer(dbCustomerId, dbCustomerFirstName, dbCustomerLastName, dbCustomerCity, dbCustomerZip);
                 customerList.add(dbCustomerList);
-
             }
-
-        }catch(SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
         }
         return customerList;
     }
 
+    /**
+     * Update customer information by customer ID.
+     *
+     * @param c The Customer object with updated information.
+     */
     public void updateCustomer(Customer c) {
-        try{
+        try {
             PreparedStatement ps = conn.prepareStatement("update customer set first_name=?, last_name=?, city=?, zip_code=? where customer_id=?");
             ps.setString(1, c.getCustomerFirstName());
             ps.setString(2, c.getCustomerLastName());
@@ -266,21 +301,25 @@ public class InventoryDAO {
             ps.setInt(4, c.getCustomerZip());
             ps.setInt(5, c.getCustomerId());
             ps.executeUpdate();
-        }catch(SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
+    /**
+     * Add a new customer to the customer table.
+     *
+     * @param c The Customer object to be added.
+     */
     public void addCustomer(Customer c) {
         try {
             PreparedStatement ps = conn.prepareStatement("insert into customer (first_name, last_name, city, zip_code) values (?, ?, ?, ?)");
-
             ps.setString(1, c.getCustomerFirstName());
             ps.setString(2, c.getCustomerLastName());
             ps.setString(3, c.getCustomerCity());
             ps.setInt(4, c.getCustomerZip());
             ps.executeUpdate();
-        }catch (SQLException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
